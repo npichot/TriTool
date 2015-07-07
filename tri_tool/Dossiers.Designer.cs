3bc12394890e2652d4b33605c4c65f030df6284c
Nicolas Pichot <pichot.n@gmail.com>
2015-07-07 11:02:49 +0200

    Edition of the WF Dossiers.cs

--- tri_tool/Dossiers.Designer.cs
+++ tri_tool/Dossiers.Designer.cs
@@ -1,4 +1,4 @@
-﻿namespace tri_tool
+﻿namespace TriTool
 {
     partial class Dossiers
     {
@@ -28,11 +28,142 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.label1 = new System.Windows.Forms.Label();
+            this.pathMusique = new System.Windows.Forms.TextBox();
+            this.browseMusique = new System.Windows.Forms.Button();
+            this.valider = new System.Windows.Forms.Button();
+            this.openFolder = new System.Windows.Forms.FolderBrowserDialog();
+            this.browsePhotos = new System.Windows.Forms.Button();
+            this.pathPhotos = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.browseVideos = new System.Windows.Forms.Button();
+            this.pathVideos = new System.Windows.Forms.TextBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(13, 13);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(286, 13);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Veuillez définir un emplacement pour stocker votre musique";
+            // 
+            // pathMusique
+            // 
+            this.pathMusique.Location = new System.Drawing.Point(13, 30);
+            this.pathMusique.Name = "pathMusique";
+            this.pathMusique.Size = new System.Drawing.Size(400, 20);
+            this.pathMusique.TabIndex = 1;
+            // 
+            // browseMusique
+            // 
+            this.browseMusique.Location = new System.Drawing.Point(442, 28);
+            this.browseMusique.Name = "browseMusique";
+            this.browseMusique.Size = new System.Drawing.Size(75, 23);
+            this.browseMusique.TabIndex = 2;
+            this.browseMusique.Text = "Browse";
+            this.browseMusique.UseVisualStyleBackColor = true;
+            this.browseMusique.Click += new System.EventHandler(this.browseMusique_Click);
+            // 
+            // valider
+            // 
+            this.valider.Location = new System.Drawing.Point(442, 183);
+            this.valider.Name = "valider";
+            this.valider.Size = new System.Drawing.Size(75, 23);
+            this.valider.TabIndex = 3;
+            this.valider.Text = "Valider";
+            this.valider.UseVisualStyleBackColor = true;
+            this.valider.Click += new System.EventHandler(this.valider_Click);
+            // 
+            // browsePhotos
+            // 
+            this.browsePhotos.Location = new System.Drawing.Point(442, 81);
+            this.browsePhotos.Name = "browsePhotos";
+            this.browsePhotos.Size = new System.Drawing.Size(75, 23);
+            this.browsePhotos.TabIndex = 6;
+            this.browsePhotos.Text = "Browse";
+            this.browsePhotos.UseVisualStyleBackColor = true;
+            this.browsePhotos.Click += new System.EventHandler(this.browsePhotos_Click);
+            // 
+            // pathPhotos
+            // 
+            this.pathPhotos.Location = new System.Drawing.Point(12, 81);
+            this.pathPhotos.Name = "pathPhotos";
+            this.pathPhotos.Size = new System.Drawing.Size(400, 20);
+            this.pathPhotos.TabIndex = 5;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(12, 64);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(272, 13);
+            this.label2.TabIndex = 4;
+            this.label2.Text = "Veuillez définir un emplacement pour stocker vos photos";
+            // 
+            // browseVideos
+            // 
+            this.browseVideos.Location = new System.Drawing.Point(443, 133);
+            this.browseVideos.Name = "browseVideos";
+            this.browseVideos.Size = new System.Drawing.Size(75, 23);
+            this.browseVideos.TabIndex = 9;
+            this.browseVideos.Text = "Browse";
+            this.browseVideos.UseVisualStyleBackColor = true;
+            this.browseVideos.Click += new System.EventHandler(this.browseVideos_Click);
+            // 
+            // pathVideos
+            // 
+            this.pathVideos.Location = new System.Drawing.Point(13, 133);
+            this.pathVideos.Name = "pathVideos";
+            this.pathVideos.Size = new System.Drawing.Size(400, 20);
+            this.pathVideos.TabIndex = 8;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(13, 116);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(271, 13);
+            this.label3.TabIndex = 7;
+            this.label3.Text = "Veuillez définir un emplacement pour stocker vos vidéos";
+            // 
+            // Dossiers
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(529, 223);
+            this.Controls.Add(this.browseVideos);
+            this.Controls.Add(this.pathVideos);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.browsePhotos);
+            this.Controls.Add(this.pathPhotos);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.valider);
+            this.Controls.Add(this.browseMusique);
+            this.Controls.Add(this.pathMusique);
+            this.Controls.Add(this.label1);
+            this.Name = "Dossiers";
             this.Text = "Dossiers";
+            this.Load += new System.EventHandler(this.Dossiers_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
         }
 
         #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox pathMusique;
+        private System.Windows.Forms.Button browseMusique;
+        private System.Windows.Forms.Button valider;
+        private System.Windows.Forms.FolderBrowserDialog openFolder;
+        private System.Windows.Forms.Button browsePhotos;
+        private System.Windows.Forms.TextBox pathPhotos;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Button browseVideos;
+        private System.Windows.Forms.TextBox pathVideos;
+        private System.Windows.Forms.Label label3;
     }
 }
--- tri_tool/Dossiers.cs
+++ tri_tool/Dossiers.cs
@@ -5,16 +5,68 @@ using System.Data;
 using System.Drawing;
 using System.Linq;
 using System.Text;
-using System.Threading.Tasks;
 using System.Windows.Forms;
+using tri_tool.Properties;
+using System.IO;
 
-namespace tri_tool
+namespace TriTool
 {
     public partial class Dossiers : Form
     {
         public Dossiers()
         {
             InitializeComponent();
+
+            pathMusique.Text = Settings.Default.Musique;
+            pathPhotos.Text = Settings.Default.Photos;
+            pathVideos.Text = Settings.Default.Videos;
+        }
+
+        private void browseMusique_Click(object sender, EventArgs e)
+        {
+            openFolder.SelectedPath = pathMusique.Text;
+            openFolder.ShowDialog();
+            pathMusique.Text = openFolder.SelectedPath;
+        }
+
+        private void valider_Click(object sender, EventArgs e)
+        {
+            if (!Directory.Exists(pathMusique.Text) || !Directory.Exists(pathPhotos.Text) || !Directory.Exists(pathVideos.Text))
+                MessageBox.Show("Veuillez revoir les chemins définis.", "Chemin introuvable", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            else
+            {
+                Settings.Default.Musique = pathMusique.Text;
+                Settings.Default.Photos = pathPhotos.Text;
+                Settings.Default.Videos = pathVideos.Text;
+                Settings.Default.initFolder = true;
+                Settings.Default.Save();
+                Close();
+            }
+        }
+
+        private void Dossiers_Load(object sender, EventArgs e)
+        {
+            this.FormClosing += new FormClosingEventHandler(Dossiers_FormClosing);
+        }
+
+        private void Dossiers_FormClosing(object sender, FormClosingEventArgs e)
+        {
+            if (!Settings.Default.initFolder)
+                e.Cancel = true;
+        }
+
+        private void browsePhotos_Click(object sender, EventArgs e)
+        {
+            openFolder.SelectedPath = pathPhotos.Text;
+            openFolder.ShowDialog();
+            pathPhotos.Text = openFolder.SelectedPath;
+        }
+
+        private void browseVideos_Click(object sender, EventArgs e)
+        {
+            openFolder.SelectedPath = pathVideos.Text;
+            openFolder.ShowDialog();
+            pathVideos.Text = openFolder.SelectedPath;
         }
     }
 }
